9c4dea372232bf7ef76c5e8251574bbecfe1c304
CJ <csh.joo77@gmail.com>
2024-03-25 22:05:34 -0400

    Added the const for inquirer and FS. Listed a couple of questions to be asked during the readme development based off of previous readme files

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type : 'input',
+        name : 'title',
+        message: 'What is the title of your Project?'
+    },
+    {
+        type : 'input',
+        name : 'description',
+        message : 'Provide a description of your project : '
+    },
+    {
+        type : 'input',
+        name : 'Tools',
+        message : 'What tools and Packages did you have to use for this project? : '
+    },
+    {
+        type : 'input',
+        name : 'Links',
+        message : 'Please provide links to your project : '
+    },
+    {
+        type : 'input',
+        name : 'Credits',
+        Message : 'Please list anyone you wish to give credit to : '
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
